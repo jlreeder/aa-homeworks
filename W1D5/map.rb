068c7abb2918da4e825c5f327fa9ccd879a7691c
Jason Reeder <info@jasonreeder.com>
2017-01-26 23:49:51 -0800

    Initial commit

---
+++ W1D5/map.rb
@@ -0,0 +1,38 @@
+class Map
+  def initialize
+    @contents = []
+  end
+
+  def assign(key, val)
+  end
+
+  def lookup(key)
+    idx = @contents.index { |pair| pair.first == key }
+    @contents[idx].last
+  end
+
+  def remove(key)
+  end
+
+  def show
+    p @contents
+  end
+end
+
+def map_tests
+  puts "Initialize map"
+  map = Map.new
+  puts "\nAssign 4 to 'kangaroo'"
+  map.assign(4, 'kangaroo')
+  map.show
+  puts "Lookup value at 4"
+  map.lookup(4)
+  puts "\nAssign 7 to 'wallaby'"
+  map.assign(7, 'wallaby')
+  map.show
+  puts "\nRemove 4 from map"
+  map.remove(4)
+  map.show
+end
+
+map_tests if __FILE__ == $PROGRAM_NAME
