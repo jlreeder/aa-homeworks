85f756ee787c6f536dc9c210dfa06e822b92ae22
Jason Reeder <info@jasonreeder.com>
2017-02-01 21:12:40 -0800

    Add dominant_octopus

--- W2D4/big_o_ctopus.rb
+++ W2D4/big_o_ctopus.rb
@@ -1,5 +1,18 @@
 class Array
 
+  def dominant_octopus
+    merge_sort.last
+  end
+
+  def merge_sort
+    return self if length <= 1
+    midpoint = length / 2
+    left = take(midpoint).merge_sort
+    right = drop(midpoint).merge_sort
+
+    Array.merge(left, right)
+  end
+
   def clever_octopus
     longest = ''
 
@@ -7,12 +20,31 @@ class Array
 
     longest
   end
+
+  def self.merge(left, right)
+    merged = []
+
+    until left.empty? || right.empty?
+      if left.first.length < right.first.length
+        merged << left.shift
+      else
+        merged << right.shift
+      end
+    end
+
+    merged += left
+    merged += right
+
+    merged
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
   prey = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh',
           'fsh', 'fiiiissshhhhhh']
 
+  puts 'Dominant octopus:'
+  puts prey.dominant_octopus
   puts 'Clever octopus:'
   puts prey.clever_octopus
 end
