4f15b98822cd645a809049ed2d9aea70575278c5
Jason Reeder <info@jasonreeder.com>
2017-02-01 20:57:40 -0800

    Add clever

--- W2D4/big_o_ctopus.rb
+++ W2D4/big_o_ctopus.rb
@@ -1,6 +1,18 @@
 class Array
+
+  def clever_octopus
+    longest = ''
+
+    each { |fish| longest = fish if longest.length < fish.length }
+
+    longest
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  prey = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+  prey = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh',
+          'fsh', 'fiiiissshhhhhh']
+
+  puts 'Clever octopus:'
+  puts prey.clever_octopus
 end
