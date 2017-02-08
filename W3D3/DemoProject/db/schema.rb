d5ae178dffd6b82ab0a8269961fcf82317a7c552
Jason Reeder <info@jasonreeder.com>
2017-02-08 07:48:36 -0800

    Add house

--- W3D3/DemoProject/db/schema.rb
+++ W3D3/DemoProject/db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208153859) do
+ActiveRecord::Schema.define(version: 20170208154524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string   "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
