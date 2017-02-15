7834bc10a2e276b71a0e852f9c978192fa628a55
Jason Reeder <info@jasonreeder.com>
2017-02-15 09:39:17 -0800

    Add user methods

--- W4D3/auth-practice/app/models/user.rb
+++ W4D3/auth-practice/app/models/user.rb
@@ -15,4 +15,28 @@ class User < ActiveRecord::Base
   validates :password_digest, presence: { message: "Password can't be blank" }
   validates :password, length: { minimum: 7, allow_nil: true }
   before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    User.where(username: username, password: password)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    @session_token = User.generate_session_token
+    self.save
+  end
+
+  def ensure_session_token
+    @session_token ||= User.generate_session_token
+  end
+
+  def password=(provided_password)
+    @password = provided_password
+    @password_digest = BCrypt::Password.create(provided_password)
+  end
 end
