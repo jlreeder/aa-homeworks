e63e519b55baf2dbce2f7430559699549520f10a
Jason Reeder <info@jasonreeder.com>
2017-02-15 09:31:50 -0800

    Add user validations

--- W4D3/auth-practice/app/models/user.rb
+++ W4D3/auth-practice/app/models/user.rb
@@ -11,4 +11,8 @@
 #
 
 class User < ActiveRecord::Base
+  validates :username, :session_token, presence: true
+  validates :password_digest, presence: { message: "Password can't be blank" }
+  validates :password, length: { minimum: 7, allow_nil: true }
+  before_validation :ensure_session_token
 end
