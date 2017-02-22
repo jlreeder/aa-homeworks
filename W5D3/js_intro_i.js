9a19d6ffa9023868b0ef33c4bc018da97b627ac8
Jason Reeder <info@jasonreeder.com>
2017-02-21 21:48:44 -0800

    Initial commit

---
+++ W5D3/js_intro_i.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// Returns 'in' twice
+function mysteryScoping1() {
+    var x = 'out of block';
+    if (true) {
+        var x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+
+// Return both 'out of' and 'in'
+function mysteryScoping2() {
+    const x = 'out of block';
+    if (true) {
+        const x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+
+// The following throws an error, since it tries to declare x twice
+// function mysteryScoping3() {
+//     const x = 'out of block';
+//     if (true) {
+//         var x = 'in block';
+//         console.log(x);
+//     }
+//     console.log(x);
+// }
+
+// Returns both 'in' and 'out'
+function mysteryScoping4() {
+    let x = 'out of block';
+    if (true) {
+        let x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+
+// Throws an error, since it tries to declare x twice
+// function mysterScoping5() {
+//     let x = 'out of block';
+//     if (true) {
+//         let x = 'in block';
+//         console.log(x);
+//     }
+//     let x = 'out of block again';
+//     console.log(x);
+// }
+
