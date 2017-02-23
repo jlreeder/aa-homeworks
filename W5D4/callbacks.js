1bb74ff32c84b9f105d1444be4dd8ccd9252fdbb
Jason Reeder <info@jasonreeder.com>
2017-02-23 09:36:59 -0800

    Add comments

--- W5D4/callbacks.js
+++ W5D4/callbacks.js
@@ -1,7 +1,11 @@
+// Meant to be copied and pasted into the browser's console, not run in Node
+
+// Simple timeout
 setTimeout(function() {
     alert("Popup");
 }, 3000);
 
+// Timeout with closure
 function hammerTime(time) {
     function announceTime() {
         alert(`${time} is hammertime!`);
