421d5cb0651bfc8ca3f626fb3a50e4a23f59d900
Jason Reeder <info@jasonreeder.com>
2017-03-06 19:21:19 -0800

    First attempt at logging

--- W7D2/frontend/todo_redux.jsx
+++ W7D2/frontend/todo_redux.jsx
@@ -4,11 +4,23 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
+const addLoggingToDispatch = (store) => (next) => (action) => {
+  const dispatch = store.dispatch;
+  console.log(store.getState());
+  console.log(action);
+
+  const result = dispatch(action);
+  console.log(store.getState());
+
+  return result;
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
+  const newStore = addLoggingToDispatch(store);
 
   const root = document.getElementById('content');
-  ReactDOM.render(<Root store={store} />, root);
+  ReactDOM.render(<Root store={newStore} />, root);
 });
