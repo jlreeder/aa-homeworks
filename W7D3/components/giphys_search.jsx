d81e20b0224324f730740c8af821fa390b86bfed
Jason Reeder <info@jasonreeder.com>
2017-03-08 08:31:35 -0800

    Complete first attempt

--- W7D3/components/giphys_search.jsx
+++ W7D3/components/giphys_search.jsx
@@ -1,3 +1,34 @@
 import React from 'react';
 
 import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Component {
+  constructor () {
+    super();
+    this.state = { searchTerm: 'words' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ searchTerm: e.currentTarget.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let searchTerm = this.state.searchTerm.split(" ").join("+");
+    this.props.fetchSearchGiphys(searchTerm);
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="search-bar">
+          <input value={this.state.searchTerm} onChange={this.handleChange} />
+          <button type="submit" onClick={this.handleSubmit}>Search</button>
+        </form>
+        <GiphysIndex giphys={this.props} />
+      </div>
+    );
+  }
+}
--- W7D3/components/giphys_search_container.jsx
+++ W7D3/components/giphys_search_container.jsx
@@ -1,3 +1,18 @@
 import { connect } from 'react-redux';
 import GiphysSearch from './giphys_search';
 import { fetchSearchGiphys } from '../actions/giphy_actions';
+
+const mapStateToProps = (state) => ({
+  giphys: state.giphys
+});
+
+const matchDispatchToProps = (dispatch) => {
+  return {
+    fetchSearchGiphys: (searchTerm) => (dispatch(fetchSearchGiphys(searchTerm)))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapStateToProps
+)(GiphysSearch);
